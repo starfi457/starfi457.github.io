47ff80cc3e2cdfe8950e192249d2cc9cddc0274a
SophiaBini <128740254+starfi457@users.noreply.github.com>
2025-01-18 22:59:20 -0300

    Update atividade04.js

--- CRUD/js/atividade04.js
+++ CRUD/js/atividade04.js
@@ -1,75 +1,181 @@
-class Veiculo {
-    constructor(marca,modelo,ano) {
-      this.marca = marca;
-      this.modelo = modelo;
-      this.ano = ano ;
+class Livro {
+    constructor(val_id,val_titulo,val_autor,val_anoPublicacao,val_disponivel){
+        this.id=val_id
+        this.titulo=val_titulo
+        this.autor=val_autor
+        this.ano=val_anoPublicacao
+        this.disponivel=val_disponivel
     }
-    descrever() {
-      const mensagem = "Marca: "+ this.marca +", Modelo: "+ this.modelo +" Ano:  "+this.ano
-      return mensagem;
+    detalhes(){
+        let mensagem="ID: "+this.id+" Título: "+this.titulo+" Autor: "+this.autor+" Ano de Publicação: "+this.ano+" Status: ";
+        if (this.disponivel){
+           mensagem=mensagem+" disponível"
+        }else{
+           mensagem=mensagem+" indisponível"
+        }
+        return mensagem;   
     }
-  }
-  class Carro extends Veiculo {
-    constructor(marca,modelo,ano,portas) {
-      super(marca,modelo,ano);
-      this.portas = portas;
-      
+    emprestar(){
+        this.disponivel=false
     }
-    show(){
-        const mensagem = "Marca: "+ this.marca +", Modelo: "+ this.modelo +" Ano:  "+this.ano + "  Portas: "+this.portas
-        return mensagem;         
+    devolver(){
+        this.disponivel=true
+    }  
+    atualizar(val_titulo,val_autor,val_anoPublicacao) {
+        this.titulo=val_titulo
+        this.autor=val_autor
+        this.ano=val_anoPublicacao      
     }
-
-    
-} 
-class Moto  extends Veiculo{
-    constructor(marca,modelo,ano,cilindradas) {
-        super(marca,modelo,ano);
-        this.cilindradas=cilindradas;
-      }
-      show(){
-          const mensagem = "Marca: "+ this.marca +", Modelo: "+ this.modelo +" Ano:  "+this.ano + "  Cilindradas: "+this.cilindradas
-          return mensagem;         
-      }
-  
 }
+class Biblioteca {
+    constructor() {
+      this.id=0;
+      this.livros=[];
+    }
+    proxid(){
+      this.id++;
+      return this.id;
+    }
+    setid(chave){
+    this.id=chave;
+    }
+    setlivros(colecao){
+        this.livros=[];
+        for (const item of colecao){
+            const ze =new Livro(item.id,item.titulo,item.autor,item.ano,item.disponivel)
+            this.livros.push(ze)
+        }
+    this.livros=colecao;
+    }
+    adicionarLivro(exemplar){
+        this.livros.push(exemplar)
+        this.salvarDados()
+    }
+   listarLivros(){
+      return this.livros
+   }
+   pesquisarLivros(busca){
+    let filtro=true
+    let resultado=[]
+    for (const item of this.livros){
+        if(busca.titulo !=""){
+            filtro= filtro && (busca.titulo==item.titulo)
+        }
+        if(busca.autor !=""){
+            filtro= filtro && (busca.autor==item.autor)
+        }
+        if(busca.ano !=""){
+            filtro= filtro && (busca.ano==item.ano)
+        }
+        if(busca.id !=""){
+            filtro= filtro && (busca.id==item.id)
+        }
+        if(filtro){
+            resultado.push(item)
+        }else {filtro=true}
+    }
+    return resultado
+   }
+   atualizarLivro(val_id, novosDados){
+    const i = this.livros.map(e => e.id).indexOf(val_id);
+    if(i != -1){
+        this.livros[i]=novosDados
+        this.salvarDados()
+        return 0;
+        }
+        else{
+        return -1;        
+        }   
+   }
+   removerLivro(val_id){
+        const i = this.livros.map(e => e.id).indexOf(val_id);
+        if(i != -1){
+        this.livros.splice(i,1);
+        this.salvarDados()
+        return 0;
+        }
+        else{
+        return -1;        
+        }
+        
+   }
+   salvarDados(){
+    let jlivros = JSON.stringify(this)
+    localStorage.setItem("Livraria",jlivros)
+   }
+
+  }
+var Livraria
+var livrinho
+
+if(localStorage.getItem("Livraria")===null){
+    Livraria=undefined
+}else{
+    Livraria=new Biblioteca
+    jlivros=JSON.parse(localStorage.getItem("Livraria"))
+    Livraria.setid(jlivros.id)
+    Livraria.setlivros(jlivros.livros)
+}
+
+
 function clean() {
-    document.querySelector("#marca").value = ""
-    document.querySelector("#modelo").value = ""
-    document.querySelector("#veiculo").value = ""
+    document.querySelector("#chave").value = ""
+    document.querySelector("#titulo").value = ""
+    document.querySelector("#autor").value = ""
     document.querySelector("#ano").value = ""
-    document.querySelector("#portas").value=""
-    document.querySelector("#cilindradas").value=""
     document.querySelector("#resultado").innerHTML=""
 }
-function show() {
-    let val_marca=document.querySelector("#marca").value
-    let val_modelo=document.querySelector("#modelo").value
-    let val_ano=document.querySelector("#ano").value
-    if(document.querySelector("#veiculo").value=="Carro"){
-    let val_portas=document.querySelector("#portas").value
-    const Ze = new Carro(val_marca,val_modelo,val_ano,val_portas)
-    const resultado = document.querySelector("#resultado")
-    resultado.innerHTML=Ze.descrever()+"<br>"+Ze.show()}
-    if(document.querySelector("#veiculo").value=="Moto"){
-    let val_cilindradas=document.querySelector("#cilindradas").value
-    const Ze = new Moto(val_marca,val_modelo,val_ano,val_cilindradas)
-    const resultado = document.querySelector("#resultado")
-    resultado.innerHTML=Ze.descrever()+"<br>"+Ze.show()
-    }
-}    
 
-function troca(){
-    if(document.querySelector("#veiculo").value=="Carro"){
-        document.getElementById('portas').style.display="block"
-        document.getElementById('Lporta').style.display="block"
-        document.getElementById('cilindradas').style.display="none"
-        document.getElementById('Lcilindrada').style.display="none"
+function pesquisar() {
+    if (Livraria!=undefined ){
+    let mensagem=""
+    let val_id=document.querySelector("#chave").value
+    let resultado= document.querySelector("#resultado")
+    mensagem=val_id 
+    if(mensagem==""){
+      alert("Informe o Id para a  busca!!!")
+    } else
+    {
+        let busca= new Livro(val_id,"","","",true)
+        let ze
+        const lista = Livraria.pesquisarLivros(busca)
+        mensagem="Foram encontrados: "+ lista.length+ " livro(s)<br/>"
+        for (const exemplar of lista){
+            ze=new Livro(exemplar.id,exemplar.titulo,exemplar.autor,exemplar.ano,exemplar.disponivel)
+            mensagem+=ze.detalhes()+"<br/>"
+        }
+        resultado.innerHTML=mensagem
+        if (lista.length==1){ 
+        livrinho=ze
+        document.querySelector("#titulo").value=ze.titulo
+        document.querySelector("#autor").value=ze.autor
+        document.querySelector("#ano").value=ze.ano
+
+        }
+        else{livrinho=undefined}
+    }  
     }
-    if(document.querySelector("#veiculo").value=="Moto"){
-        document.getElementById('portas').style.display="none"
-        document.getElementById('Lporta').style.display="none"
-        document.getElementById('cilindradas').style.display="block"
-        document.getElementById('Lcilindrada').style.display="block"
+    else{
+       alert("Biblioteca inexistente!!!")
+    }
+
+}
+
+
+function atualizar() {
+    if (Livraria!=undefined ){
+        if(livrinho!=undefined){
+            let mensagem=""
+            let val_titulo=document.querySelector("#titulo").value
+            let val_autor=document.querySelector("#autor").value
+            let val_anoPublicacao=document.querySelector("#ano").value
+            livrinho.atualizar(val_titulo,val_autor,val_anoPublicacao)
+            Livraria.atualizarLivro(livrinho.id,livrinho)
+            let resultado= document.querySelector("#resultado")
+            resultado.innerHTML=livrinho.detalhes()
+        }
+    }
+    else{
+       alert("Biblioteca inexistente!!!")
     }
 }
