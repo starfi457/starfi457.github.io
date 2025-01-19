aa907562547822204874934af5c5491c2715ca1b
SophiaBini <128740254+starfi457@users.noreply.github.com>
2025-01-19 09:37:35 -0300

    Update atividade01.js

--- CRUD/js/atividade01.js
+++ CRUD/js/atividade01.js
@@ -1,10 +1,10 @@
 class Livro {
-  constructor(val_id,val_titulo,val_autor,val_anoPublicacao){
+  constructor(val_id,val_titulo,val_autor,val_anoPublicacao,val_disponivel){
       this.id=val_id
       this.titulo=val_titulo
       this.autor=val_autor
       this.ano=val_anoPublicacao
-      this.disponivel=true
+      this.disponivel=val_disponivel
   }
   detalhes(){
       let mensagem="ID: "+this.id+" Título: "+this.titulo+" Autor: "+this.autor+" Ano de Publicação: "+this.ano+" Status: ";
@@ -21,45 +21,85 @@ class Livro {
   devolver(){
       this.disponivel=true
   }  
-  atualizar(val_titulo,val_autor,val_anoPublicacao) {
+  atualizar(val_titulo,val_autor,val_anoPublicacao,val_disponivel) {
       this.titulo=val_titulo
       this.autor=val_autor
-      this.ano=val_anoPublicacao      
+      this.ano=val_anoPublicacao   
+      this.disponivel=val_disponivel   
   }
 }
 class Biblioteca {
-    constructor() {
-      this.id=0;
-      this.livros=[];
-    }
-    proxid(){
-      this.id++;
-      return this.id;
-    }
-    setid(chave){
-    this.id=chave;
-    }
-    setlivros(colecao){
-    this.livros=colecao;
-    }
+  constructor() {
+    this.id=0;
+    this.livros=[];
   }
-  let Livraria
+  proxid(){
+    this.id++;
+    return this.id;
+  }
+  setid(chave){
+  this.id=chave;
+  }
+  setlivros(colecao){
+      this.livros=[];
+      for (const item of colecao){
+          let exemplar=new Livro(item.id,item.titulo,item.autor,item.ano,item.disponivel)
+          this.livros.push(exemplar)
+      }
+  }
+  adicionarLivro(exemplar){
+      this.livros.push(exemplar)
+      this.salvarDados()
+  }
+ listarLivros(){
+    return this.livros
+ }
+ atualizarLivro(id, novosDados){
+  const i = this.livros.map(e => e.id).indexOf(pesquisa);
+  if(i != -1){
+      this.livros[i].atualizar(novosDados.titulo,novosDados.autor,novosDados.ano)
+      this.salvarDados()
+      return 0;
+      }
+      else{
+      return -1;        
+      }   
+ }
+ removerLivro(pesquisa){
+      const i = this.livros.map(e => e.id).indexOf(pesquisa);
+      if(i != -1){
+      this.livros.splice(i,1);
+      this.salvarDados()
+      return 0;
+      }
+      else{
+      return -1;        
+      }
+      
+ }
+ salvarDados(){
+  let jlivros = JSON.stringify(this)
+  localStorage.setItem("Livraria",jlivros)
+ }
+
+}
+let Livraria
 function criar(){
-Livraria=new Biblioteca;
-localStorage.clear();
-const resultado = document.querySelector("#resultado");
-resultado.innerHTML="Biblioteca criada com sucesso";
+  Livraria=new Biblioteca;
+  localStorage.clear();
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML="Biblioteca criada com sucesso";
 }
 function salvar(){
-if (Livraria!=undefined){
-const jlivros= JSON.stringify(Livraria)
-localStorage.setItem(  "Livraria",jlivros)
-const resultado = document.querySelector("#resultado")
-resultado.innerHTML="Biblioteca salva com sucesso <br/>";
-}
-else{
-alert ("Biblioteca não Criada!")
-}
+  if (Livraria!=undefined){
+  const jlivros= JSON.stringify(Livraria)
+  localStorage.setItem(  "Livraria",jlivros)
+  const resultado = document.querySelector("#resultado")
+  resultado.innerHTML="Biblioteca salva com sucesso <br/>";
+  }
+  else{
+  alert ("Biblioteca não Criada!")
+  }
 }
 function carregar(){
   if (localStorage.getItem("Livraria")===null){
@@ -73,8 +113,8 @@ function carregar(){
   const resultado = document.querySelector("#resultado")
   resultado.innerHTML="Biblioteca carregada com sucesso <br/>";
   } 
-  }
-  function apagar(){
+}
+function apagar(){
     if (localStorage.getItem("Livraria")===null){
       alert ("Biblioteca não carregada!") 
     }
@@ -84,4 +124,17 @@ function carregar(){
     const resultado = document.querySelector("#resultado")
     resultado.innerHTML="Biblioteca apagada com sucesso <br/>";
     } 
+}
+function listar(){
+  const resultado = document.querySelector("#resultado")
+  if (Livraria!=undefined){
+    let mensagem=""
+    for (const item of Livraria.listarLivros()){
+       mensagem += item.detalhes() + "<br>";
     }
+    resultado.innerHTML=mensagem
+  }
+  else{
+    alert ("Biblioteca não Criada!")
+    }  
+}
